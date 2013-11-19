e44d4110bd3b248b50ad43fbd6c235a35a8f3c63
mirkoperillo <mirko.perillo@gmail.com>
2013-11-19 15:50:03 +0100

    added comment to sql table

--- src/main/resources/placex_solr.sql
+++ src/main/resources/placex_solr.sql
@@ -5,3 +5,5 @@ CREATE TABLE placex_solr AS SELECT *
   WHERE (placex.place_id IN ( SELECT max(placex.place_id) AS max
            FROM placex
           GROUP BY placex.parent_place_id, placex.name -> 'name'::text, placex.class, placex.type));
+          
+COMMENT ON TABLE placex_solr IS 'table use to create solr index, only a way describing the same street'
